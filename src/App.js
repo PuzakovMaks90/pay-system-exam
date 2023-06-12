c3da612644b7001eecb8cd6d62ae67141ddce9d6
Puzakov Maxym <m.puzakov90@gmail.com>
2023-06-12 22:35:21 +0300

    add main files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Route, Routes } from "react-router-dom";
+import Layout from './components/Layout';
+import Home from './components/Home';
+import NotFound from './components/NotFound';
+import Cards from './components/Cards';
+import ContrAgents from './components/ContrAgents';
+import History from './components/History';
+import Single from './components/Single';
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+     <Routes>
+      <Route path='/pay_system/' element={<Layout/>}>
+        <Route index element={<Home/>}/>
+        <Route path='/pay_system/cards' element={<Cards/>}/>
+        <Route path='/pay_system/contrAgents' element={<ContrAgents/>}/>
+        <Route path='/pay_system/history' element={<History/>}/>
+        <Route path='/pay_system/:id' element={<Single/>}/>
+        <Route path='*' element={<NotFound/>}/>
+      </Route>
+      </Routes>
+    </BrowserRouter>
+ 
+      
+  
+      
+
   );
 }
 
